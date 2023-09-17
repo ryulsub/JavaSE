530d845e9b3a6d4d694b5f00ae563554d3b5c8de
dbftj <dbftj@192.168.1.6>
2023-09-18 00:05:41 +0900

    ryul

--- src/section04/Conditional01.java
+++ src/section04/Conditional01.java
@@ -8,10 +8,10 @@ package section04;
  * 1. if문
  * 	if(조건식) {
  * 		조건식 true 일 때 실행 명령문
- * }
+ * 	}
  * 
  *  if(조건식)
- *  	조건식 true 일 때 실행 명령문(명령문이 하나 일경우 중괄호 생략 가능)
+ *  	조건식 ture 일 때 실행 명령문(명령문이 하나 일경우 중괄호 생략 가능)
  * 
  */
 public class Conditional01 {
@@ -25,12 +25,12 @@ public class Conditional01 {
 			System.out.println("num은 홀수 입니다.");
 		}
 		
-		if(num % 2 == 0) {
+		if(num % 2 == 0) 
 			System.out.println("num은 짝수 입니다.");
-	
 		
-		if(num % 2 != 0) {
+		if(num % 2 != 0) 
 			System.out.println("num은 홀수 입니다.");
-		}
 		
 	}
+
+}
