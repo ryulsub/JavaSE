27b9f0fac0f18959cad0cb56de266480330dde0d
dbftj <dbftj@172.30.1.87>
2023-10-06 17:17:49 +0900

    ryul

--- src/section09/Constructor03.java
+++ src/section09/Constructor03.java
@@ -5,20 +5,21 @@ public class Constructor03 {
 		AniCharacter aniChar1 = new AniCharacter();
 		aniChar1.printInfo();
 		
+		System.out.println("=========================");
 		String[] items = {"또치", "도우너", "마이콜"};
 		AniCharacter aniChar2 = new AniCharacter(
-											"둘리",
-											"아기공룡 둘리",
-											"ENFP",
+											"둘리", 
+											"아기공룡 둘리", 
+											"ENFP", 
 											items
 											);
-		
 		aniChar2.printInfo();
 		
-		System.out.println("===============================");
+		System.out.println("==========================");
 		
-		String[] friends = {"진구", "이슬이", "퉁퉁이", "비실이")};
+		String[] friends = {"진구", "이슬이", "퉁퉁이", "비실이"};
 		new AniCharacter(friends);
+		
 		
 	}
 	
--- src/section10/Americano.java
+++ src/section10/Americano.java
@@ -4,14 +4,16 @@ public class Americano extends Espresso {
 	
 	public int water;
 	
-	public Americano(String bean, int water) {
+	public Americano(String bean, String brand, int water) {
 		super(bean);
-		this.brand = brand;
+		super.brand = brand;
 		this.water = water;
 	}
 	
-	@override
-	public void printInfo(0 ) {
-		super.pirntInfo();
+	@Override
+	public void printInfo() {
+		super.printInfo();
+		System.out.println("water: " + water);
 	}
+	
 }
